7a2adddc64ad60f5cceedb2f65887b902d163512
ProgrammerJ1 <scotte1953@gmail.com>
2022-06-04 10:05:03 -0400

    Made members public

--- FileAnalysis.hpp
+++ FileAnalysis.hpp
@@ -5,20 +5,21 @@
 #include <map>
 using namespace std;
 class Analysis {
-    const string filename;
-    size_t bytes;
-    size_t blocks;
-    size_t blocksize;
-    char filetype;
-    short int DeviceNumber;
-    int inodenumber;
-    short int NumberofHardLinks;
-    bool Readable;
-    bool Writeable;
-    bool Executable;
-    time_t AccessedLast;
-    time_t ModifiedLast;
-    time_t ChangedLast;
-    time_t BirthDate;
-    map<string,string> InodeTableData;
+    public:
+        const char* filename;
+        size_t bytes;
+        size_t blocks;
+        size_t blocksize;
+        char filetype;
+        short int DeviceNumber;
+        int inodenumber;
+        short int NumberofHardLinks;
+        bool Readable;
+        bool Writeable;
+        bool Executable;
+        time_t AccessedLast;
+        time_t ModifiedLast;
+        time_t ChangedLast;
+        time_t BirthDate;
+        map<string,string> InodeTableData;
 };
