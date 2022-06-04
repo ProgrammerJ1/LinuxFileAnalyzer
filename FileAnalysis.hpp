f4e77a80742ac5b55eeb07e61beba0baa65cfc59
ProgrammerJ1 <scotte1953@gmail.com>
2022-06-04 08:48:20 -0400

    Added FileAnalysis class and function

---
+++ FileAnalysis.hpp
@@ -0,0 +1,24 @@
+#include <cstddef>
+#include <cstdbool>
+#include <ctime>
+#include <string>
+#include <map>
+using namespace std;
+class Analysis {
+    const string filename;
+    size_t bytes;
+    size_t blocks;
+    size_t blocksize;
+    char filetype;
+    short int DeviceNumber;
+    int inodenumber;
+    short int NumberofHardLinks;
+    bool Readable;
+    bool Writeable;
+    bool Executable;
+    time_t AccessedLast;
+    time_t ModifiedLast;
+    time_t ChangedLast;
+    time_t BirthDate;
+    map<string,string> InodeTableData;
+};
