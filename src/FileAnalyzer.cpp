41755805e2c243c8e7599f9ff44fc9a3e74245b7
ProgrammerJ1 <scotte1953@gmail.com>
2022-06-05 10:13:53 -0400

    Removed code from if and impleenting special information

--- src/FileAnalyzer.cpp
+++ src/FileAnalyzer.cpp
@@ -46,50 +46,71 @@ int Program(char* File,bool GUI) {
         analysisoffile.ChangedLast=FileStat.st_ctim;
         analysisoffile.OnwerId=FileStat.st_uid;
         analysisoffile.GroupId=FileStat.st_gid;
-        if (analysisoffile.filetype==0) {
-            uint8_t* FileContents;
-            FILE* fp=fopen(File,"rb");
-            fread(FileContents,analysisoffile.bytes,sizeof(uint8_t),fp);
-            fclose(fp);
-            printf("Filename: %s\n",analysisoffile.filename);
-            printf("Amount of Bytes: %u\n",analysisoffile.bytes);
-            printf("Amount of Blocks: %u\n",analysisoffile.blocks);
-            printf("Block Size: %u\n",analysisoffile.blocksize);
-            printf("%s","File Type: Regular\n");
-            printf("Device Id: 0x%X\n",analysisoffile.DeviceNumber);
-            printf("Inode Number: %u",analysisoffile.inodenumber);
-            printf("Amount of Hard Links: %u",analysisoffile.NumberofHardLinks);
-            printf("%s","Permissions: ");
-            uint8_t AmountofPerms=0;
-            if (analysisoffile.Readable) {
-                printf("%s","Readable");
+        printf("Filename: %s\n",analysisoffile.filename);
+        printf("Amount of Bytes: %u\n",analysisoffile.bytes);
+        printf("Amount of Blocks: %u\n",analysisoffile.blocks);
+        printf("Block Size: %u\n",analysisoffile.blocksize);
+        switch (analysisoffile.filetype) {
+            case 0:
+                printf("%s","File Type: Regular\n");
+            case 1:
+                printf("%s","File Type: Directory");
+            case 2:
+                printf("%s","File Type: Block Device");
+            case 3:
+                printf("%s","File Type: Character Device");
+            case 4:
+                printf("%s","File Type: Pipe");
+            case 5:
+                printf("%s","File Type: Link");
+            case 6:
+                printf("%s","File Type: Socket");
+        }
+        printf("Device Id: 0x%X\n",analysisoffile.DeviceNumber);
+        printf("Inode Number: %u",analysisoffile.inodenumber);
+        printf("Amount of Hard Links: %u",analysisoffile.NumberofHardLinks);
+        printf("%s","Permissions: ");
+        uint8_t AmountofPerms=0;
+        if (analysisoffile.Readable) {
+            printf("%s","Readable");
+            AmountofPerms+=1;
+        } else if (analysisoffile.Writeable) {
+            if (AmountofPerms>1) {
+                printf("%s",", Writable");
+            } else {
+                printf("%s","Writeable");
                 AmountofPerms+=1;
-            } else if (analysisoffile.Writeable) {
-                if (AmountofPerms>1) {
-                    printf("%s",", Writable");
-                } else {
-                    printf("%s","Writeable");
-                    AmountofPerms+=1;
-                }
-            } else if (analysisoffile.Executable) {
-                if (AmountofPerms>1) {
-                    printf("%s",", Executable");
-                } else {
-                    printf("Executable");
-                }
             }
-            AmountofPerms=NULL;
-            printf("%c",'\n');
-            struct tm TrueAccessTime;
-            localtime_r(&analysisoffile.AccessedLast.tv_sec,&TrueAccessTime);
-            printf("Last time accessed: %s, %s %s, %u %u:%u:%u %s",getWeekTable().find(TrueAccessTime.tm_wday)->second,getMonthTable().find(TrueAccessTime.tm_mon),FormatNumbertoDateNumber(TrueAccessTime.tm_mday),TrueAccessTime.tm_year+1900,TrueAccessTime.tm_hour,TrueAccessTime.tm_min,TrueAccessTime.tm_sec,TrueAccessTime.tm_zone);
-            struct tm TrueModifiyTime;
-            localtime_r(&analysisoffile.ModifiedLast.tv_sec,&TrueModifiyTime);
-            printf("Last time modified: %s, %s %s, %u %u:%u:%u %s",getWeekTable().find(TrueModifiyTime.tm_wday)->second,getMonthTable().find(TrueModifiyTime.tm_mon),FormatNumbertoDateNumber(TrueModifiyTime.tm_mday),TrueModifiyTime.tm_year+1900,TrueModifiyTime.tm_hour,TrueModifiyTime.tm_min,TrueModifiyTime.tm_sec,TrueModifiyTime.tm_zone);
-            struct tm TrueChangeTime;
-            localtime_r(&analysisoffile.ChangedLast.tv_sec,&TrueChangeTime);
-            printf("Last time changed: %s, %s %s, %u %u:%u:%u %s",getWeekTable().find(TrueChangeTime.tm_wday)->second,getMonthTable().find(TrueChangeTime.tm_mon),FormatNumbertoDateNumber(TrueChangeTime.tm_mday),TrueChangeTime.tm_year+1900,TrueChangeTime.tm_hour,TrueChangeTime.tm_min,TrueChangeTime.tm_sec,TrueChangeTime.tm_zone);
-            struct tm TrueModifiyTime;
+        } else if (analysisoffile.Executable) {
+            if (AmountofPerms>1) {
+                printf("%s",", Executable");
+            } else {
+                printf("Executable");
+            }
+        }
+        AmountofPerms=NULL;
+        printf("%c",'\n');
+        struct tm TrueAccessTime;
+        localtime_r(&analysisoffile.AccessedLast.tv_sec,&TrueAccessTime);
+        printf("Last time accessed: %s, %s %s, %u %u:%u:%u %s\n",getWeekTable().find(TrueAccessTime.tm_wday)->second,getMonthTable().find(TrueAccessTime.tm_mon),FormatNumbertoDateNumber(TrueAccessTime.tm_mday),TrueAccessTime.tm_year+1900,TrueAccessTime.tm_hour,TrueAccessTime.tm_min,TrueAccessTime.tm_sec,TrueAccessTime.tm_zone);
+        struct tm TrueModifiyTime;
+        localtime_r(&analysisoffile.ModifiedLast.tv_sec,&TrueModifiyTime);
+        printf("Last time modified: %s, %s %s, %u %u:%u:%u %s\n",getWeekTable().find(TrueModifiyTime.tm_wday)->second,getMonthTable().find(TrueModifiyTime.tm_mon),FormatNumbertoDateNumber(TrueModifiyTime.tm_mday),TrueModifiyTime.tm_year+1900,TrueModifiyTime.tm_hour,TrueModifiyTime.tm_min,TrueModifiyTime.tm_sec,TrueModifiyTime.tm_zone);
+        struct tm TrueChangeTime;
+        localtime_r(&analysisoffile.ChangedLast.tv_sec,&TrueChangeTime);
+        printf("Last time changed: %s, %s %s, %u %u:%u:%u %s\n",getWeekTable().find(TrueChangeTime.tm_wday)->second,getMonthTable().find(TrueChangeTime.tm_mon),FormatNumbertoDateNumber(TrueChangeTime.tm_mday),TrueChangeTime.tm_year+1900,TrueChangeTime.tm_hour,TrueChangeTime.tm_min,TrueChangeTime.tm_sec,TrueChangeTime.tm_zone);
+        struct tm TrueModifiyTime;
+        printf("%s","Would you like to print file type specific information? [Y/n]");
+        char Choice=getchar();
+        bool SpecialInfo;
+        switch (Choice) {
+            case 'Y':
+                SpecialInfo=true;
+            case defualt:
+                SpecialInfo=false;
+        }
+        if (SpecialInfo) {
+            
         }
     } else {
         perror("Error: File not accessible");
