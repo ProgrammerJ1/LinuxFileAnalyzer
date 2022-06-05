a48da165fb8da1540ef76a963ca658a61ffcbca8
ProgrammerJ1 <scotte1953@gmail.com>
2022-06-05 12:17:59 -0400

    Added Regular FIle and Directory Anylzer

--- src/FileAnalyzer.cpp
+++ src/FileAnalyzer.cpp
@@ -7,6 +7,8 @@
 #include "FileAnalysis.hpp"
 #include "MonthTable.hpp"
 #include <WeekTable.hpp>
+#include <dirent.h>
+#include <libgen.h>
 int main(int argc,char** argv) {
     if (argc==1) {
         perror("Error: No file to analyze");
@@ -46,71 +48,103 @@ int Program(char* File,bool GUI) {
         analysisoffile.ChangedLast=FileStat.st_ctim;
         analysisoffile.OnwerId=FileStat.st_uid;
         analysisoffile.GroupId=FileStat.st_gid;
-        printf("Filename: %s\n",analysisoffile.filename);
-        printf("Amount of Bytes: %u\n",analysisoffile.bytes);
-        printf("Amount of Blocks: %u\n",analysisoffile.blocks);
-        printf("Block Size: %u\n",analysisoffile.blocksize);
-        switch (analysisoffile.filetype) {
-            case 0:
-                printf("%s","File Type: Regular\n");
-            case 1:
-                printf("%s","File Type: Directory");
-            case 2:
-                printf("%s","File Type: Block Device");
-            case 3:
-                printf("%s","File Type: Character Device");
-            case 4:
-                printf("%s","File Type: Pipe");
-            case 5:
-                printf("%s","File Type: Link");
-            case 6:
-                printf("%s","File Type: Socket");
-        }
-        printf("Device Id: 0x%X\n",analysisoffile.DeviceNumber);
-        printf("Inode Number: %u",analysisoffile.inodenumber);
-        printf("Amount of Hard Links: %u",analysisoffile.NumberofHardLinks);
-        printf("%s","Permissions: ");
-        uint8_t AmountofPerms=0;
-        if (analysisoffile.Readable) {
-            printf("%s","Readable");
-            AmountofPerms+=1;
-        } else if (analysisoffile.Writeable) {
-            if (AmountofPerms>1) {
-                printf("%s",", Writable");
-            } else {
-                printf("%s","Writeable");
+        if (GUI) {
+            printf("Filename: %s\n",analysisoffile.filename);
+            printf("Amount of Bytes: %u\n",analysisoffile.bytes);
+            printf("Amount of Blocks: %u\n",analysisoffile.blocks);
+            printf("Block Size: %u\n",analysisoffile.blocksize);
+            switch (analysisoffile.filetype) {
+                case 0:
+                    printf("%s","File Type: Regular\n");
+                case 1:
+                    printf("%s","File Type: Directory");
+                case 2:
+                    printf("%s","File Type: Block Device");
+                case 3:
+                    printf("%s","File Type: Character Device");
+                case 4:
+                    printf("%s","File Type: Pipe");
+                case 5:
+                    printf("%s","File Type: Link");
+                case 6:
+                    printf("%s","File Type: Socket");
+            }
+            printf("Device Id: 0x%X\n",analysisoffile.DeviceNumber);
+            printf("Inode Number: %u",analysisoffile.inodenumber);
+            printf("Amount of Hard Links: %u",analysisoffile.NumberofHardLinks);
+            printf("%s","Permissions: ");
+            uint8_t AmountofPerms=0;
+            if (analysisoffile.Readable) {
+                printf("%s","Readable");
                 AmountofPerms+=1;
+            } else if (analysisoffile.Writeable) {
+                if (AmountofPerms>1) {
+                    printf("%s",", Writable");
+                } else {
+                    printf("%s","Writeable");
+                    AmountofPerms+=1;
+                }
+            } else if (analysisoffile.Executable) {
+                if (AmountofPerms>1) {
+                    printf("%s",", Executable");
+                } else {
+                    printf("Executable");
+                }
             }
-        } else if (analysisoffile.Executable) {
-            if (AmountofPerms>1) {
-                printf("%s",", Executable");
-            } else {
-                printf("Executable");
+            AmountofPerms=NULL;
+            printf("%c",'\n');
+            struct tm TrueAccessTime;
+            localtime_r(&analysisoffile.AccessedLast.tv_sec,&TrueAccessTime);
+            printf("Last time accessed: %s, %s %s, %u %u:%u:%u %s\n",getWeekTable().find(TrueAccessTime.tm_wday)->second,getMonthTable().find(TrueAccessTime.tm_mon),FormatNumbertoDateNumber(TrueAccessTime.tm_mday),TrueAccessTime.tm_year+1900,TrueAccessTime.tm_hour,TrueAccessTime.tm_min,TrueAccessTime.tm_sec,TrueAccessTime.tm_zone);
+            struct tm TrueModifiyTime;
+            localtime_r(&analysisoffile.ModifiedLast.tv_sec,&TrueModifiyTime);
+            printf("Last time modified: %s, %s %s, %u %u:%u:%u %s\n",getWeekTable().find(TrueModifiyTime.tm_wday)->second,getMonthTable().find(TrueModifiyTime.tm_mon),FormatNumbertoDateNumber(TrueModifiyTime.tm_mday),TrueModifiyTime.tm_year+1900,TrueModifiyTime.tm_hour,TrueModifiyTime.tm_min,TrueModifiyTime.tm_sec,TrueModifiyTime.tm_zone);
+            struct tm TrueChangeTime;
+            localtime_r(&analysisoffile.ChangedLast.tv_sec,&TrueChangeTime);
+            printf("Last time changed: %s, %s %s, %u %u:%u:%u %s\n",getWeekTable().find(TrueChangeTime.tm_wday)->second,getMonthTable().find(TrueChangeTime.tm_mon),FormatNumbertoDateNumber(TrueChangeTime.tm_mday),TrueChangeTime.tm_year+1900,TrueChangeTime.tm_hour,TrueChangeTime.tm_min,TrueChangeTime.tm_sec,TrueChangeTime.tm_zone);
+            struct tm TrueModifiyTime;
+            printf("%s","Would you like to print file type specific information? [Y/n]");
+            printf("Group Id Owner: %u",analysisoffile.GroupId);
+            printf("Owner ID: %u",analysisoffile.OnwerId);
+            char Choice=getchar();
+            bool SpecialInfo;
+            switch (Choice) {
+                case 'Y':
+                    SpecialInfo=true;
+                default:
+                    SpecialInfo=false;
             }
-        }
-        AmountofPerms=NULL;
-        printf("%c",'\n');
-        struct tm TrueAccessTime;
-        localtime_r(&analysisoffile.AccessedLast.tv_sec,&TrueAccessTime);
-        printf("Last time accessed: %s, %s %s, %u %u:%u:%u %s\n",getWeekTable().find(TrueAccessTime.tm_wday)->second,getMonthTable().find(TrueAccessTime.tm_mon),FormatNumbertoDateNumber(TrueAccessTime.tm_mday),TrueAccessTime.tm_year+1900,TrueAccessTime.tm_hour,TrueAccessTime.tm_min,TrueAccessTime.tm_sec,TrueAccessTime.tm_zone);
-        struct tm TrueModifiyTime;
-        localtime_r(&analysisoffile.ModifiedLast.tv_sec,&TrueModifiyTime);
-        printf("Last time modified: %s, %s %s, %u %u:%u:%u %s\n",getWeekTable().find(TrueModifiyTime.tm_wday)->second,getMonthTable().find(TrueModifiyTime.tm_mon),FormatNumbertoDateNumber(TrueModifiyTime.tm_mday),TrueModifiyTime.tm_year+1900,TrueModifiyTime.tm_hour,TrueModifiyTime.tm_min,TrueModifiyTime.tm_sec,TrueModifiyTime.tm_zone);
-        struct tm TrueChangeTime;
-        localtime_r(&analysisoffile.ChangedLast.tv_sec,&TrueChangeTime);
-        printf("Last time changed: %s, %s %s, %u %u:%u:%u %s\n",getWeekTable().find(TrueChangeTime.tm_wday)->second,getMonthTable().find(TrueChangeTime.tm_mon),FormatNumbertoDateNumber(TrueChangeTime.tm_mday),TrueChangeTime.tm_year+1900,TrueChangeTime.tm_hour,TrueChangeTime.tm_min,TrueChangeTime.tm_sec,TrueChangeTime.tm_zone);
-        struct tm TrueModifiyTime;
-        printf("%s","Would you like to print file type specific information? [Y/n]");
-        char Choice=getchar();
-        bool SpecialInfo;
-        switch (Choice) {
-            case 'Y':
-                SpecialInfo=true;
-            case defualt:
-                SpecialInfo=false;
-        }
-        if (SpecialInfo) {
-            
+            if (SpecialInfo) {
+                switch (analysisoffile.filetype) {
+                    case 0:
+                        uint8_t* Contents;
+                        FILE* fp=fopen(File,"rb");
+                        fread(Contents,1,FileStat.st_size,fp);
+                        fclose(fp);
+                        printf("%s","File Contents: ");
+                        for (size_t i=0;i<sizeof(Contents);i++) {
+                            printf("%X",Contents[i]);
+                            if (i+1!=sizeof(Contents)) {
+                                printf("%c",' ');
+                            }
+                        }
+                        return 0;
+                    case 1:
+                        printf("%s","Directory Contents\n________________");
+                        DIR *d;
+                        struct dirent *dir;
+                        d=opendir(File);
+                        if (d) {
+                            while ((dir = readdir(d)) != NULL) {
+                                printf("%s",dir->d_name);
+                            }
+                        }
+                        closedir(d);
+                    case 2:
+                        if
+                }
+            }
+            return 0;
         }
     } else {
         perror("Error: File not accessible");
